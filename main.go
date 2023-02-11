406e42a030f0a562edc6f0599affb0a2f5dee33a
wenlin42 <wenlin42@icloud.com>
2023-02-11 16:37:38 +0800

    Integrated Gin

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	// New a gin engine instance
+	r := gin.New()
+
+	// Register a middleware
+	r.Use(gin.Logger(), gin.Recovery())
+
+	// Register a route
+	r.GET("/", func(c *gin.Context) {
+
+		// Response  in JSON format
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+
+	// Handling 404 requests
+	r.NoRoute(func(c *gin.Context) {
+		// Get the Accept information of the header information
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			// HTML
+			c.String(http.StatusNotFound, "The page returns 404")
+		} else {
+			// JSON
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "The route is not defined, please confirm whether the url and request method are correct.",
+			})
+		}
+	})
+
+	// Run service, ports is 8000
+	r.Run(":8000")
 }
