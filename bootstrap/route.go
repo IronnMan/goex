809454d3b9aefd3580cb0905aaba904d227341eb
wenlin42 <wenlin42@icloud.com>
2023-02-12 14:23:20 +0800

    init route

---
+++ bootstrap/route.go
@@ -0,0 +1,42 @@
+package bootstrap
+
+import (
+	"github.com/gin-gonic/gin"
+	"goex/routes"
+	"net/http"
+	"strings"
+)
+
+// SetupRoute route initialization
+func SetupRoute(r *gin.Engine) {
+
+	registerGlobalMiddleWare(r)
+
+	routes.RegisterAPIRoutes(r)
+
+	setup404Handler(r)
+}
+
+func registerGlobalMiddleWare(r *gin.Engine) {
+	r.Use(
+		gin.Logger(),
+		gin.Recovery(),
+	)
+}
+
+func setup404Handler(r *gin.Engine) {
+
+	r.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			// HTML
+			c.String(http.StatusNotFound, "The page returns 404")
+		} else {
+			// JSON
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "The route is not defined, please confirm whether the url and request method are correct.",
+			})
+		}
+	})
+}
--- main.go
+++ main.go
@@ -1,43 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"strings"
+	"goex/bootstrap"
 )
 
 func main() {
-	// New a gin engine instance
-	r := gin.New()
 
-	// Register a middleware
-	r.Use(gin.Logger(), gin.Recovery())
+	router := gin.New()
 
-	// Register a route
-	r.GET("/", func(c *gin.Context) {
+	bootstrap.SetupRoute(router)
 
-		// Response  in JSON format
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "World!",
-		})
-	})
-
-	// Handling 404 requests
-	r.NoRoute(func(c *gin.Context) {
-		// Get the Accept information of the header information
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			// HTML
-			c.String(http.StatusNotFound, "The page returns 404")
-		} else {
-			// JSON
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "The route is not defined, please confirm whether the url and request method are correct.",
-			})
-		}
-	})
-
-	// Run service, ports is 8000
-	r.Run(":8000")
+	err := router.Run(":3000")
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
 }
