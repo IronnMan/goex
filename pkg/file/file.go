79ba124b318a707f1295757e66c1793f3d81e2a6
wenlin42 <wenlin42@icloud.com>
2023-02-27 15:48:14 +0800

    add make cmd

---
+++ pkg/file/file.go
@@ -0,0 +1,20 @@
+package file
+
+import "os"
+
+// Put save data to file
+func Put(data []byte, to string) error {
+	err := os.WriteFile(to, data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Exists check if file exists
+func Exists(fileToCheck string) bool {
+	if _, err := os.Stat(fileToCheck); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
