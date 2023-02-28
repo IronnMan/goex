1983fd1c748fe06e1cf6a0578ceff5ed8114899d
wenlin42 <wenlin42@icloud.com>
2023-02-28 11:01:22 +0800

    add migrate lib

---
+++ pkg/migrate/migrator.go
@@ -0,0 +1,38 @@
+package migrate
+
+import (
+	"goex/pkg/database"
+	"gorm.io/gorm"
+)
+
+type Migrator struct {
+	Folder   string
+	DB       *gorm.DB
+	Migrator gorm.Migrator
+}
+
+func (m Migrator) createMigrationsTable() {
+	migration := Migration{}
+
+	if !m.Migrator.HasTable(&migration) {
+		m.Migrator.CreateTable(&migration)
+	}
+}
+
+type Migration struct {
+	ID        uint64 `gorm:"primaryKey;autoIncrement;"`
+	Migration string `gorm:"type:varchar(255);not null;unique;"`
+	Batch     int
+}
+
+func NewMigrator() *Migrator {
+	migrator := &Migrator{
+		Folder:   "database/migrations/",
+		DB:       database.DB,
+		Migrator: database.DB.Migrator(),
+	}
+
+	migrator.createMigrationsTable()
+
+	return migrator
+}
